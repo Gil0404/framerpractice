cac77744b8a3177bfac44163ac07553cb6b660df
Gil0404 <it.gilbertdgarcia@gmail.com>
2024-10-24 18:45:38 +0800

    clearing navbar

--- src/components/reusable/navbar.tsx
+++ src/components/reusable/navbar.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import StaggeredDropDown from "./menu";
 
 function NavBar() {
   return (
     <>
-      <div className="w-screen h-16 fixed z-50  sm:hidden xs:hidden md:flex lg:flex xl:flex "></div>
+      <div className="w-screen h-16 fixed z-50  sm:hidden xs:hidden md:flex lg:flex xl:flex ">
+        <div>hellow</div>
+      </div>
 
       <div className="w-screen h-16 fixed z-50  sm:flex xs:flex  md:hidden lg:hidden xl:hidden "></div>
     </>
